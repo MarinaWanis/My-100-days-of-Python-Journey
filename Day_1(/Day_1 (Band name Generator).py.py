93245f5d3cce6d7bf1e80c67b1cf33a460f640f1
Marina Maged <52045845+MarinaWanis@users.noreply.github.com>
2023-02-26 16:53:32 +0400

    Create Day_1 (Band name Generator).py.py
    Day 1 
    Band Name Generator

---
+++ Day_1(/Day_1 (Band name Generator).py.py
@@ -0,0 +1,8 @@
+#Day 1 Challenge - Band Name Generator
+print("Welcome to the Band Name Generator.")
+
+city = input("What's the name of the city you grew up in?\n")
+
+pet_name = input("What's your pet's name?\n")
+
+print("Your brand name could be " + city +Dubai " " + pet_name)

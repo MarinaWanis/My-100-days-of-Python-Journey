ee1889750eb394f016f997dd778789eeadca0228
Marina Maged <52045845+MarinaWanis@users.noreply.github.com>
2023-02-26 17:03:50 +0400

    Delete Day_1( directory

--- Day_1(/Day_1 (Band name Generator).py
+++
@@ -1,8 +0,0 @@
-#Day 1 Challenge - Band Name Generator
-print("Welcome to the Band Name Generator.")
-
-city = input("What's the name of the city you grew up in?\n")
-
-pet_name = input("What's your pet's name?\n")
-
-print("Your brand name could be " + city +Dubai " " + pet_name)
